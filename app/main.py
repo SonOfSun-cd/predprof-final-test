1ce1c07fcbcd2246ee4b1177b1d530a64daf395f
SonOfSun-cd <fedorasticheck@gmail.com>
2026-03-08 21:56:19 +0300

    ав

--- app/main.py
+++ app/main.py
@@ -10,27 +10,26 @@ def generate_flight_task(
     min_points: int = 3, 
     max_points: int = 10, 
     max_distance: float = 50.0
-) -> List[List[float]]:
+):
     num_points = random.randint(min_points, max_points)
     mission = []
     
     for _ in range(num_points):
         distance = round(random.uniform(1.0, max_distance), 2)
-        cargo = _generate_valid_cargo()
+        cargo = generate_valid_cargo()
         
         mission.append([distance, cargo])
         
     return mission
 
-task = generate_flight_task()
-
-def _generate_valid_cargo() -> int:
+def generate_valid_cargo():
     if random.random() < 0.5:
         return random.randint(1, 7)
     else:
         n = random.randint(1, 7)
         return 8 * (2 ** n) - 8
 
+task = generate_flight_task()
 
 @app.get("/ppo_it_final")
 async def root():
